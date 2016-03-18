2800e26a4a31f7f50181719542c16bf9eab677eb
MsEmma <emmaphiri@hotmail.com>
2016-03-18 17:58:49 +0200

    most popular product

--- process_weekly.js
+++ process_weekly.js
@@ -1,53 +1,95 @@
-//module getsalesList input filepath
+#!/usr/bin/node
 
-var fs = require('fs');
+exports.getSalesList = function(filepath) {
 
-var inputSales = fs.readFileSync('./input/week1.csv', "utf8");
-inputSales = inputSales.replace("Day,Date,stock item,No sold,Sales Price\n", "")
+  var fs = require('fs');
 
-var interimArray = inputSales.split('\n');
-processedArray = [];
+  var inputSales = fs.readFileSync(filepath, "utf8");
+  inputSales = inputSales.replace("Day,Date,stock item,No sold,Sales Price\n", "")
 
+  var interimArray = inputSales.split('\n');
+  var processedArray = [];
 
-for (i = 0; i < interimArray.length - 1; i++) {
-  processedArray.push(interimArray[i].split(","));
-}
-
-var salesList = [];
-
-processedArray.forEach(function(array) {
-  salesList.push([array[2], Number(array[3])]);
-})
-
-
-//module getweeklySales input salesList
-
-salesList.sort();
-
-var weeklySales = [];
-
-for (i = 1; i < salesList.length; i++) {
-
-  if (i % 7 === 0) {
-    var stockItem = salesList[i][0];
-    var sold = 0;
-
-    for (j = 0; j < 7; j++) {
-      sold += salesList[i + j][1];
-    }
-    weeklySales.push({
-      "stockItem": stockItem,
-      "sold": sold
-    });
+  for (i = 0; i < interimArray.length; i++) {
+    processedArray.push(interimArray[i].split(","));
   }
+
+  for (var i = processedArray.length - 1; i >= 0; i--) {
+    if (processedArray[i][1] === "1-Mar") {
+      processedArray.splice(i, 1);
+    }
+  }
+
+  var salesList = [];
+
+  processedArray.forEach(function(array) {
+    salesList.push([array[2], Number(array[3])]);
+  })
+
+  return salesList;
 }
 
-// module getmostpopularproduct input weekly sales
+exports.getWeeklySales = function(salesList) {
 
-weeklySales.sort(function(a, b) {
-  return b.sold - a.sold;
-});
+  var weeklySales = {};
 
-var mostPopularProduct = weeklySales[0];
+  salesList.forEach(function(array) {
+    if (!weeklySales.hasOwnProperty(array[0])) {
+      weeklySales[array[0]] = array[1];
+    } else {
+      weeklySales[array[0]] += array[1];
+    }
+  })
 
-console.log(mostPopularProduct);
+  return weeklySales;
+}
+
+exports.getPopularProduct = function(weeklySales) {
+
+  var mostSold = 0;
+
+  for (var product in weeklySales) {
+    if (weeklySales[product] > mostSold) {
+      mostSold = weeklySales[product];
+    }
+  };
+
+  var popularProduct = {};
+
+  for (var product in weeklySales) {
+    if (weeklySales[product] === mostSold) {
+      popularProduct = {
+        "The most popular product is": product,
+        "Quantities sold": mostSold
+      }
+    };
+  }
+
+  return popularProduct;
+}
+
+exports.getLeastPopularProduct = function(weeklySales) {
+
+  var sold = [];
+
+  for (var product in weeklySales) {
+    sold.push(weeklySales[product]);
+  };
+
+  var leastSold = Math.min.apply(null, sold);
+
+  var leastPopularProduct = {};
+
+  for (var product in weeklySales) {
+    if (weeklySales[product] === leastSold) {
+      leastPopularProduct = {
+        "The least popular product is": product,
+        "Quantities sold": leastSold
+      }
+    }
+  };
+
+  return leastPopularProduct;
+}
+
+// console.log(process.argv.slice(2));
--- test/process_weekly_test.js
+++ test/process_weekly_test.js
@@ -1,11 +1,22 @@
 var assert = require('assert');
-var process_data =require('../process_data')
+var process_weekly =require('../process_weekly')
 
+describe("process_weekly", function(){
 
-describe("process_data", function(){
+    it('should return length of processed sales list of week1', function(){
+          assert.equal(106, process_weekly.getSalesList('./input/week1.csv').length);
+    });
 
-    it('should find the lines syncronously', function(){
-          assert.equal(11, process_data.getsalesList('./input/week1.csv').length);
-    });   
+    it('should return length of processed sales list of week2', function(){
+          assert.equal(119, process_weekly.getSalesList('./input/week2.csv').length);
+    });
+
+    it('should return length of processed sales list of week3', function(){
+          assert.equal(106, process_weekly.getSalesList('./input/week3.csv').length);
+    });
+
+    it('should return length of processed sales list of week4', function(){
+          assert.equal(106, process_weekly.getSalesList('./input/week4.csv').length);
+    });
 
 });
