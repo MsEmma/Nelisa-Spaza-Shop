d4a7fc37a24ce22b0be6d7e2f146d36239c180c2
MsEmma <emmaphiri@hotmail.com>
2016-05-31 17:08:57 +0200

    aggregation and styling

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
     db_categories = require('./routes/db_categories'),
     db_sales = require('./routes/db_sales'),
     db_purchases = require('./routes/db_purchases'),
-    weekly = require('./routes/weekly'),
+    summary = require('./routes/summary'),
     process_weekly_sales = require("./process_weekly_sales"),
     process_weekly_purchases = require("./process_weekly_purchases"),
     categories = require('./categories');
@@ -57,39 +57,39 @@ app.get('/aboutus', function(req, res) {
     res.render('aboutus');
 });
 
-app.get('/stats/:week', function(req, res) {
-
-    var week = req.params.week;
-    var filepath = './input/' + week + '.csv';
-
-    var sales = process_weekly_sales.getSalesList(filepath);
-    var weekly_sales = process_weekly_sales.getWeeklySales(sales);
-    var selling_prices = process_weekly_sales.getSellPrices(sales);
-    var most_popular = process_weekly_sales.getPopularProduct(weekly_sales);
-    var least_popular = process_weekly_sales.getLeastPopularProduct(weekly_sales);
-
-    var purchases = process_weekly_purchases.getPurchases('./input/purchases.csv');
-    var weekly_purchases = process_weekly_purchases.getWeeklyPurchases(purchases, week);
-    var cost_prices = process_weekly_purchases.getCostPrices(weekly_purchases);
-    var total_profit = process_weekly_purchases.getTotalProfit(cost_prices, selling_prices, weekly_sales);
-    var most_profitable_product = process_weekly_purchases.getMostProfitableProduct(total_profit);
-
-    var category_map = categories.getCategories('./input/categories.csv');
-    var category_sales = categories.getCatSales(category_map, weekly_sales);
-    var most_popular_cat = categories.getMostPopularCategory(category_sales);
-    var least_popular_cat = categories.getLeastPopularCategory(category_sales);
-    var cat_profit = categories.getCatProfit(category_map, total_profit);
-    var most_profitable_cat = categories.getMostProfitableCategory(cat_profit);
-
-    var result = {
-        Week: week.match(/\d+/),
-        pop: [most_popular, least_popular, most_popular_cat, least_popular_cat],
-        profit: [most_profitable_product, most_profitable_cat]
-    };
-
-    res.render('display', result);
-
-});
+// app.get('/stats/:week', function(req, res) {
+//
+//     var week = req.params.week;
+//     var filepath = './input/' + week + '.csv';
+//
+//     var sales = process_weekly_sales.getSalesList(filepath);
+//     var weekly_sales = process_weekly_sales.getWeeklySales(sales);
+//     var selling_prices = process_weekly_sales.getSellPrices(sales);
+//     var most_popular = process_weekly_sales.getPopularProduct(weekly_sales);
+//     var least_popular = process_weekly_sales.getLeastPopularProduct(weekly_sales);
+//
+//     var purchases = process_weekly_purchases.getPurchases('./input/purchases.csv');
+//     var weekly_purchases = process_weekly_purchases.getWeeklyPurchases(purchases, week);
+//     var cost_prices = process_weekly_purchases.getCostPrices(weekly_purchases);
+//     var total_profit = process_weekly_purchases.getTotalProfit(cost_prices, selling_prices, weekly_sales);
+//     var most_profitable_product = process_weekly_purchases.getMostProfitableProduct(total_profit);
+//
+//     var category_map = categories.getCategories('./input/categories.csv');
+//     var category_sales = categories.getCatSales(category_map, weekly_sales);
+//     var most_popular_cat = categories.getMostPopularCategory(category_sales);
+//     var least_popular_cat = categories.getLeastPopularCategory(category_sales);
+//     var cat_profit = categories.getCatProfit(category_map, total_profit);
+//     var most_profitable_cat = categories.getMostProfitableCategory(cat_profit);
+//
+//     var result = {
+//         Week: week.match(/\d+/),
+//         pop: [most_popular, least_popular, most_popular_cat, least_popular_cat],
+//         profit: [most_profitable_product, most_profitable_cat]
+//     };
+//
+//     res.render('display', result);
+//
+// });
 
 app.get('/products', products.show);
 app.get('/products/add', products.showAdd);
@@ -119,8 +119,10 @@ app.get('/purchases/edit/:id', db_purchases.get);
 app.post('/purchases/update/:id', db_purchases.update);
 app.get('/purchases/delete/:id', db_purchases.delete);
 
-app.get('/weekly', weekly.showPopular);
-app.post('/weekly/stats', weekly.getStats);
+app.get('/getsummary', function(req, res) {
+    res.render('getsummary');
+});
+app.post('/summary', summary.showPopular);
 
 app.use(errorHandler);
 
--- routes/summary.js
+++ routes/summary.js
@@ -1,42 +1,54 @@
 exports.showPopular = function(req, res, next) {
-   var end_date = req.params.sale_date;
-   var start_date = '2016-02-21';
+
+    var start_date = (req.body.from).toString();
+    var end_date = (req.body.to).toString();
+
     req.getConnection(function(err, connection) {
         if (err) return next(err);
         connection.query(`SELECT products.product, SUM( sales.sold ) AS quantity
         FROM sales
         INNER JOIN products ON sales.product_id = products.id
         INNER JOIN categories ON products.category_id = categories.id
-        WHERE sales.date BETWEEN ` + start_date + ` AND ?
+        WHERE sales.date
+        BETWEEN  ?
+        AND ?
         GROUP BY products.product
         ORDER BY quantity DESC
-        LIMIT 0,1`, end_date ,function(err, most_popular) {
+        LIMIT 0,1`, [start_date, end_date], function(err, most_popular) {
             connection.query(`SELECT products.product, SUM( sales.sold ) AS quantity
           FROM sales
           INNER JOIN products ON sales.product_id = products.id
           INNER JOIN categories ON products.category_id = categories.id
-          WHERE sales.date BETWEEN ` + start_date + ` AND ?
+          WHERE sales.date
+          BETWEEN  ?
+          AND ?
           GROUP BY products.product
           ORDER BY quantity ASC
-          LIMIT 0,1`, end_date,function(err, least_popular) {
+          LIMIT 0,1`, [start_date, end_date], function(err, least_popular) {
                 connection.query(`SELECT  categories.category, sum(sales.sold) AS quantity
             FROM sales
             INNER JOIN products ON sales.product_id = products.id
             INNER JOIN categories ON products.category_id = categories.id
-            WHERE sales.date BETWEEN ` + start_date + ` AND ?
+            WHERE sales.date
+            BETWEEN  ?
+            AND ?
             GROUP BY categories.category
             ORDER BY quantity DESC
-            LIMIT 0,1`, end_date, function(err, most_popular_cat) {
+            LIMIT 0,1`, [start_date, end_date], function(err, most_popular_cat) {
                     connection.query(`SELECT  categories.category, sum(sales.sold) AS quantity
                   FROM sales
                   INNER JOIN products ON sales.product_id = products.id
                   INNER JOIN categories ON products.category_id = categories.id
-                  WHERE sales.date BETWEEN ` + start_date + ` AND ?
+                  WHERE sales.date
+                  BETWEEN  ?
+                  AND ?
                   GROUP BY categories.category
                   ORDER BY quantity ASC
-                  LIMIT 0,1`, end_date, function(err, least_popular_cat) {
+                  LIMIT 0,1`, [start_date, end_date], function(err, least_popular_cat) {
                         if (err) return next(err);
-                        res.render('weekly', {
+                        res.render('summary', {
+                            start_date,
+                            end_date,
                             most_popular,
                             least_popular,
                             most_popular_cat,
@@ -45,24 +57,6 @@ exports.showPopular = function(req, res, next) {
                     });
                 });
             });
-        });
-    });
-};
-
-exports.add = function(req, res, next) {
-    req.getConnection(function(err, connection) {
-        if (err) return next(err);
-        var data = {
-            date: req.body.date,
-            shop: req.body.shop,
-            product_id: Number(req.body.product_id),
-            quantity: Number(req.body.quantity),
-            cost: Number(req.body.cost)
-        };
-
-        connection.query('insert into purchases set ?', data, function(err, results) {
-            if (err) return next(err);
-            res.redirect('/purchases');
         });
     });
 };
