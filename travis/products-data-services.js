b6cd86b6aecd6fbd5345e73c99aa5e630770159a
MsEmma <emmaphiri@hotmail.com>
2016-07-05 20:11:38 +0200

    further Travis testing

--- travis/products-data-services.js
+++ travis/products-data-services.js
@@ -1,9 +1,7 @@
 module.exports = function(connection) {
     this.show = function(cb) {
 
-        connection.query(`SELECT products.id, products.product, categories.category
-          FROM products
-          INNER JOIN categories ON products.category_id = categories.id`, function(err, products) {
+        connection.query('SELECT products.id, products.product, categories.category FROM products INNER JOIN categories ON products.category_id = categories.id', function(err, products) {
             if (err) return cb(err, null);
             if (products && products.length > 0) {
                 return cb(null, products);
