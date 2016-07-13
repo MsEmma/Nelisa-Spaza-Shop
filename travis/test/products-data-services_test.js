a756f947c887f0230e3d29a0d65925d24d8254f5
MsEmma <emmaphiri@hotmail.com>
2016-07-13 15:49:24 +0200

    test.sql

--- travis/test/products-data-services_test.js
+++ travis/test/products-data-services_test.js
@@ -6,13 +6,13 @@ describe('test the ProductsDataService', function() {
 
     const url = process.env.MYSQL_URL !== null ? process.env.MYSQL_URL : 'mysql://root:nelisa@localhost/test';
 
-    var connection = mysql.createConnection(url);  
+    var connection = mysql.createConnection(url);
 
     it("should should count the number of products in the products table", function(done) {
 
         connection.query('select count(*) as productCount from products', function(err, products) {
             console.log(err);
-            assert.equal(20, products[0].productCount);
+            assert.equal(10, products[0].productCount);
             done();
         });
     });
