099c92fb46ec7b7a140434d479ee8b23e003e26d
MsEmma <emmaphiri@hotmail.com>
2016-07-14 20:25:39 +0200

    products add and delete tests

--- db_tests/products-data-services_test.js
+++ db_tests/products-data-services_test.js
@@ -16,26 +16,34 @@ describe('test the ProductsDataServices', function() {
             });
     });
 
-    // it('should return product list length', function(done) {
-    //     var productsDataServices = new ProductsDataServices(connection);
-    //     var data = {
-    //         product: 'Sprite',
-    //         category_id: 5
-    //     };
-    //
-    //     return productsDataServices.add(data)
-    //         .then(function(products) {
-    //             assert.equal(10, products.length);
-    //             done();
-    //         });
-    // });
+    it('should return the new product list length after adding product', function(done) {
+        var productsDataServices = new ProductsDataServices(connection);
+        var data = {
+            product: 'Sprite',
+            category_id: 5
+        };
+        return productsDataServices.add(data)
+            .then(function() {
+                productsDataServices.show()
+                    .then(function(products) {
+                        assert.equal(11, products.length);
+                        done();
+                    });
+            });
+    });
 
-    it("should should count the number of products in the products table", function(done) {
-
-        connection.query('select count(*) as productCount from products', function(err, products) {
-            console.log(err);
-            assert.equal(10, products[0].productCount);
-            done();
-        });
+    it('should return the new product list length after deleting product', function(done) {
+        var productsDataServices = new ProductsDataServices(connection);
+        var id = 11;
+        return productsDataServices.delete(id)
+            .then(function() {
+                productsDataServices.show()
+                    .then(function(products) {
+                        assert.equal(10, products.length);
+                        done();
+                    });
+            });
     });
 });
+
+// ALTER TABLE `products` AUTO_INCREMENT = 11;
